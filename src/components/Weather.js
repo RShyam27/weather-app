a2a0d2ae5aac98f671c76e8b65b2c698c3efc209
shyamraja <t7bhsh00@students.oamk.fi>
2019-02-10 17:51:49 +0200

    first commit

---
+++ src/components/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+class Weather extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                {this.props.city && this.props.country && <p className="weather__key">Location: <span className="weather__value">{this.props.city} , {this.props.country}</span></p> }
+                {this.props.tempraure && <p className="weather__key">Temprature: <span className="weather__value">{this.props.tempraure}</span></p>}
+                {this.props.humidity && <p className="weather__key">Humidity: <span className="weather__value">{this.props.humidity}</span></p>}
+                {this.props.description && <p className="weather__key">Condtions:<span className="weather__value">{this.props.description}</span> </p>}
+            </div>
+        )
+    }
+}
+export default Weather;
